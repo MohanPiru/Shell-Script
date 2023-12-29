49465d8937c0fdb0840f3fdff1f5572ef2e3ee6d
Pirumohan Mandal <140044323+MohanPiru@users.noreply.github.com>
2023-12-30 00:57:05 +0530

    Creating  Disk-space.sh file

---
+++ Disk-space.sh
@@ -0,0 +1,20 @@
+# getting free_disk_space in percentage  
+
+free_disk_space=$(df -H | egrep -v "Filesystem|tmpfs" | grep "sda3" | awk '{print $5}' | tr -d % )
+
+# to alert in email (1st set up postfix )
+
+#TO=<provide your email after setting up postfix>
+
+#set your threshold value
+threshold=50
+
+if [ $free_disk_space -gt $threshold ]
+then
+  echo "warning"
+  echo "diskspace is running out of memory : available space is $free_disk_space %" 
+# mail -s "disk space alert !" $TO
+else
+  echo "all good"
+fi
+
