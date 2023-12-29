9c76608568b9a494b34a4006984e3a8ae779465a
Pirumohan Mandal <140044323+MohanPiru@users.noreply.github.com>
2023-12-30 00:29:27 +0530

    Update create-user.sh
    updating the file

--- create-user.sh
+++ create-user.sh
@@ -1,52 +1,60 @@
 # create user with command line arguments
 
 
+#check the script is running as root or not
+#root is is always 0
 
-
-#check root access
-
-if [[ "${UID}" -ne 0 ]]
-then 
-   echo "please run with root "
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 
    exit 1
 fi
 
-# check arguments if 0 or not
+# check number os arguments if 0 or not
 
-if [[ "${#}" -lt 1 ]]
+if [ "${#}" -lt 1 ]
 then 
    echo "please give arguments"
    exit 1
 fi
 
+#taking 1st argument as username and others as comments
+
 USER_NAME="${1}"
 shift
 comment="${@}"
 
+#setting a random password 
+
 PASSWORD=$(date +%s%N)
+
+#creating user with the comment 
 
 useradd -c "$comment" -m $USER_NAME
 
-#check user is created or not
 
-if [[ $? -ne 0 ]]
+#check user is created or not
+# '$?' checks whether the previous command is successful or not(0 means successful)
+
+if [ $? -ne 0 ]
 then 
   echo "account not created "
   exit 1 
 fi
 
-echo $PASSWORD | passwd --stdin &USER_NAME
+#setting password for the user 
 
-if [[ $? -ne 0 ]]
+echo "$USER_NAME:$PASSWORD" | chpasswd
+
+if [ $? -ne 0 ]
 then 
   echo "password not set "
   exit 1 
 fi
 
-# force password change
+
+# force to change password after the first login
 
 passwd -e $USER_NAME
 
 echo "user name : $USER_NAME"
 echo " password : $PASSWORD "
- 
